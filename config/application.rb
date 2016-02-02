a85c2c823a04fa6c8723457d37350d2964756bc3
Connor Chan <connorjosephchan@gmail.com>
2016-02-02 15:16:53 -0500

    test

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 
 module AdDisplay
   class Application < Rails::Application
+     OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
+     OpenURI::Buffer.const_set 'StringMax', 0
      config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
