011272cd03c696d3e8446adef0804242e8c0543a
Connor Chan <connorjosephchan@gmail.com>
2016-02-03 13:18:14 -0500

    testing pdfs

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,6 +8,19 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf"
+else
+  # Linux (check your processor for Intel x86 or AMD x64)
+  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-i386"
+  # OS X
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf_darwin_x86"            
+  # Windows
+  # wkhtmltopdf_path = 'C:\Program Files/wkhtmltopdf/wkhtmltopdf.exe'
+end
+
+
 WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
@@ -15,8 +28,7 @@ WickedPdf.config = {
   #   or
   # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
   #:layout => "pdf.html",
-  :wkhtmltopdf => (Rails.env.test? || Rails.env.development? ? '/usr/local/bin/wkhtmltopdf' : Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s),
-  :exe_path => (Rails.env.test? || Rails.env.development? ? '/usr/local/bin/wkhtmlpdf' : Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s)
+  exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
